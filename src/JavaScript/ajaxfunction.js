754b8c16f2478f9afbb6ad74251c47cd98a636fb
David Smekal <david.smekal@hotmail.com>
2017-07-04 17:08:45 -0700

    added AJAX for result query confirmation box pops up after successful query AJAX also sends the checked arduino box

---
+++ src/JavaScript/ajaxfunction.js
@@ -0,0 +1,34 @@
+$('#sendButton').click(function(e){
+    e.preventDefault();
+
+    var queryBox = $('#publish').val();
+
+    var selectedCheckboxes = "";
+
+    $(":checkbox").each(function(){
+        var check = $(this).is(":checked");
+        if (check){
+            selectedCheckboxes += $(this).attr('id') + "\n";
+        }
+    });
+
+    $.ajax({
+        url: "",
+        type:"get",
+        data:{
+            query : queryBox,
+            arduino: selectedCheckboxes
+        },
+
+        success: function(result){
+
+            console.log(queryBox);
+            console.log(selectedCheckboxes);
+
+            if (queryBox != "") {
+                $("#successBox").show();
+                $("#successBox").fadeOut(3000);
+            }
+        }
+    });
+});
