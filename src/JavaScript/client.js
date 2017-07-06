4cba2ed0b146a53fd5b8fe3ef464f7ccf6e1c84b
rmcqueen <schroener1@gmail.com>
2017-07-05 23:40:43 -0700

    Incorporate query parsing AJAX request into the send() function to reduce code redundancy; improve function readability

--- src/JavaScript/client.js
+++ src/JavaScript/client.js
@@ -6,6 +6,7 @@
 var hostname = "127.0.0.1";
 var port = 8080;
 var connectedClients = [];
+
 // Create a client instance
 var client = new Paho.MQTT.Client(hostname, Number(port), "clientId");
 
@@ -21,7 +22,12 @@ client.connect({
     onSuccess: onConnect
 });
 
-// Connection
+
+/*
+* Purpose: when the client is first started, begin listening to the two main
+* topics of this project. The status channel indicating which Arduinos are
+* available to be picked from, and the queries results that are being sent back
+*/
 function onConnect() {
     client.subscribe("status/#", {
         qos: 2
@@ -32,7 +38,13 @@ function onConnect() {
 }
 
 
-// Lost connection
+/*
+* Purpose: if a connection is lost to the broker, print the error message to the
+* console to inform the user what caused the issue.
+*
+* @param Object responseObject a response indicating what went wrong with the
+* request
+*/
 function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
         console.log("onConnectionLost:" + responseObject.errorMessage);
@@ -40,7 +52,15 @@ function onConnectionLost(responseObject) {
 }
 
 
-// Arriving messages
+/*
+* Purpose: when a new message is received from the broker, determine the clientId
+* of the Arduino that is came from, as well as the messages desired topic.
+* sent in the form of: DESIRED_TOPIC/CLIENT_ID
+*
+* @param Message message a MQTT message object containing information about the
+* message received. Contains the destination/topic, and the string of information
+* it sent back. See mqttws31.js for more information
+*/
 function onMessageArrived(message) {
     destination = message.destinationName.split("/")[0];
     clientId = message.destinationName.split("/")[1];
@@ -57,59 +77,84 @@ function onMessageArrived(message) {
             connectedClients.splice($.inArray(clientId, connectedClients), 1);
             removeCheckboxes(clientId);
         }
-    } else {
+    }
+
+    if (destination == "query") {
         displayResults(clientId, message.payloadString);
+    } else {
+        console.log("No results");
     }
 }
 
 
-//Temporary until real query works properly
-function parse(s) {
-    var result = s.split(" ");
-    return result[result.length - 1];
-}
+/*
+* Purpose: the purpose of this function is to send the user's query to their
+* specified Arduinos (as outlined by the checked checkboxes). The request is
+* sent to a server for parsing.
 
+* @return null null if there is no client connected, returns a null as it
+* cannot process the request
+*/
+function send() {
+	// Get the query from the query box
+    var userQuery = $('#publish').val();
+    var selectedArduinos = [];
 
-//Sends publish message
-function send(publishClientMessage) {
-    if (!client) {
-        return;
-	}
-
-    // refresh
+	// Reset the values within the text area
     document.getElementById("publish").value = "";
 
-    var message = new Paho.MQTT.Message(parse(publishClientMessage)); //Temporary until real query works properly
-    message.qos = 2;
+	// Return if there is no available MQTT client to use
+    if (!client) {
+        return;
+    }
 
+    // Go over each checked checkbox, and append them to a list
     $("input[type=checkbox]").each(function() {
         if (this.checked) {
-            message.destinationName = this.id;
-			console.log("Sending message");
-            client.send(message);
+            selectedArduinos.push(this.id);
+        }
+    });
+	// Perform an AJAX request to the server for query parsing
+    $.ajax({
+        url: "127.0.0.1:3000/publish_query",
+        type: "get",
+        data: {
+            query: userQuery,
+            arduino: selectedArduinos
+        },
+
+        success: function(result) {
+            if (userQuery != "") {
+                $("#successBox").show();
+                $("#successBox").fadeOut(3000);
+            }
+
         }
     });
 }
 
 
-//Adds data to be stored in csv
-function appendcsvInputData(message)
-{
+/*
+* Purpose: appends a new line between each new message received from the broker
+* to enhance readability for the user when they interpret their query results
+*/
+function appendcsvInputData(message) {
     csvInputData += message + "\n";
 }
 
 
-//Downloads csv file
-function CSV_download()
-{
+/*
+* Purpose: enables a user to download the results of their query into a CSV
+* formatted file as 'results.csv'
+*/
+function CSV_download() {
     var data = csvInputData.split("\n");
     var csvData = "data:text/csv;charset=utf-8,";
 
     data.forEach(function(element, index) {
         if (index < (data.length - 1)) {
             csvData += element + "\n";
-        }
-        else {
+        } else {
             csvData += element;
         }
     });
@@ -122,8 +167,14 @@ function CSV_download()
 }
 
 
-function createCheckBoxes(name)
-{
+/*
+* Purpose: this function creates checkboxes for the user to select from. It is
+* based on whether or not there are any Arduinos connected to the INTRAnet
+*
+* @param string name represents the clientId of the Arduino, and creates a new
+* checkbox wrapped in a div
+*/
+function createCheckBoxes(name) {
     container = $("#checkbox");
     $("<input />", {
         type: "checkbox",
@@ -138,15 +189,30 @@ function createCheckBoxes(name)
 }
 
 
-function removeCheckboxes(name)
-{
+/*
+* Purpose: removes any checkboxes from the main page if the connection between
+* an Arduino and the broker is disconnected, or lost due to an Arduino crashing
+*
+* @param string name represents the clientId of the Arduino, and removes the
+* checkbox that corresponds to it
+*/
+function removeCheckboxes(name) {
     $("#" + name).remove();
     $("label[for=" + name + "]").remove();
 }
 
-// Displays the results into the result window
-function displayResults(clientId, message)
-{
+
+/*
+* Purpose: displays the results from the user's query into the "results" area
+* of the web page.
+*
+* @param string clientId the id that represents the Arduino the query result
+* came from
+*
+* @param string message the payloadString that was obtained from the user's query
+* should show what values were requsted in the database (if they exist)
+*/
+function displayResults(clientId, message) {
     if (clientId) {
         document.getElementById("resultArea").innerHTML = "Results from " +
             clientId + ": " + message;
