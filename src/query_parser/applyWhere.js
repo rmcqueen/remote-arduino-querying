7eb6b7066225b5cffe997e4b1a0f8ae3fe319c80
Spencer MacBeth <smacbeth@hotmail.ca>
2017-08-02 21:03:48 -0700

    abstraced reduction of clientTuples and application of filter operations

--- src/query_parser/applyWhere.js
+++ src/query_parser/applyWhere.js
@@ -1,15 +1,22 @@
 const whereString = 'WHERE name = spencer';
 const resultSet = {
   "attributes": {
-    "name": "String"
+    "name": "String",
+    "age": "int",
   },
   "clientTuples": {
     "Arduino1": [
-      "david",
-      "ryan",
-      "dustin",
-      "spencer"
-    ]
+      "david,28",
+      "ryan,21",
+      "dustin,30",
+      "spencer,24",
+    ],
+    "Arduino2": [
+      "david,28",
+      "ryan,21",
+      "dustin,30",
+      "spencer,24",
+    ],
   }
 }
 
@@ -25,45 +32,54 @@ function parseOperator(whereClause) {
 function parseWhere(whereClause) {
   const comparison = whereClause.split('WHERE ')[1];
   const operator = parseOperator(whereClause)
-  console.log(operator);
   const [ field, value ] = comparison.split(` ${operator} `);
   return { field: field, value: value, operator: operator };
 }
 
-function applyGreaterThan() {}
-function applyLessThan() {}
+function applyEquality(fieldValue, filterValue) {
+  return fieldValue == filterValue;
+}
 
-function applyEquality(resultSet, where) {
-  const columnNumber = Object.keys(resultSet.attributes).indexOf(where.field);
+function applyGreaterThan(fieldValue, filterValue) {
+  return fieldValue > filterValue;
+}
+
+function applyLessThan(fieldValue, filterValue) {
+  return fieldValue < filterValue;
+}
+
+function getFilterOperation(operator) {
+  switch(operator) {
+    case '=':
+    case 'like':
+      return applyEquality;
+    case '>':
+      return applyGreaterThan;
+    case '<':
+      return applyLessThan;
+  }
+}
+
+function applyWhereCondition(resultSet, where) {
+  const { field, value, operator } = where;
+  const columnNumber = Object.keys(resultSet.attributes).indexOf(field);
+  const filterOperation = getFilterOperation(operator);
   const filteredClientTuples = Object.keys(resultSet.clientTuples).reduce((acc, client) => {
     // split tuples on ,
     // apply where to the ith tuple, where i = index of operand in attributes.keys
     const filteredTuples = resultSet.clientTuples[client].filter(tuple => {
       const fieldValue = tuple.split(',')[columnNumber]; // assumes tuples are always ordered the same way as attributes.keys() in resultSet
-      return fieldValue == where.value;
+      return filterOperation(fieldValue, value);
     });
     return Object.assign(acc, { [client]: filteredTuples });
   }, {});
   return Object.assign({}, resultSet, { clientTuples: filteredClientTuples });
 }
 
-function applyWhere(resultSet, where) {
-  switch(where.operator) {
-    case '=':
-    case 'like':
-      return applyEquality(resultSet, where);
-    case '>':
-      return applyGreaterThan(resultSet, where);
-    case '<':
-      return applyLessThan(resultSet, where);
-  }
-}
-
-function where(resultSet, whereString) {
+function applyWhere(resultSet, whereString) {
   const where = parseWhere(whereString);
-  console.log(where);
-  return applyWhere(resultSet, where);
+  return applyWhereCondition(resultSet, where);
 }
 
-const filteredResultSet = where(resultSet, whereString);
+const filteredResultSet = applyWhere(resultSet, whereString);
 console.log(filteredResultSet);
